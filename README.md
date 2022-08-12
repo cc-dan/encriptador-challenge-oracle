1e4dde6fc218c74772efa630388fd90a26a6e198
Dante <dantecocciolo@gmail.com>
2022-08-11 23:43:58 -0300

    redesign

---
+++ README.md
@@ -0,0 +1,10 @@
+# Encriptador / Desencriptador
+### Challenge Oracle Next Education
+
+![desktop](/screenshots/desktop.png)
+
+
+![tablet](/screenshots/tablet.png)
+
+
+![mobile](/screenshots/mobile.png)
---
+++ app.js
@@ -0,0 +1,49 @@
+const diccionario = {
+    'e' : 'enter',
+    'i' : 'imes',
+    'a' : 'ai',
+    'o' : 'ober',
+    'u' : 'ufat'
+}
+
+function encriptador(entrada, desencriptar) {
+    entrada = entrada.toLowerCase();
+    let salida = "";
+
+    for (let x = 0; x < entrada.length; x++) {
+        let conversion = entrada[x];
+
+        for (let letra in diccionario) {
+            if (conversion == letra) {
+                conversion = diccionario[letra];
+
+                if (desencriptar) {
+                    conversion = letra;
+                    x += diccionario[letra].length-1;
+                }
+
+                break;
+            }
+        }
+
+        salida += conversion;
+    }
+
+    document.getElementById('encriptador').textContent = salida;
+
+    if (salida.length > 0) { 
+        document.getElementById('placeholder').style = 'display:none';
+        document.getElementById('copiar').style = 'display:inline';
+    } else {
+        document.getElementById('placeholder').style = 'display:inline-block';
+        document.getElementById('copiar').style = 'display:none';
+    }
+
+    return salida;
+}
+
+function copiar(texto) {
+    if (texto.length > 0) {
+        navigator.clipboard.writeText(texto);
+    }
+}
